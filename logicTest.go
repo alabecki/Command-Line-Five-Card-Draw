d02ba376b7a40297a459a44c7dd78c4fb49817de
Adam Labecki <adamlabecki@gmail.com>
2017-11-21 22:11:49 -0800

    Add files via upload

--- logicTest.go
+++ logicTest.go
@@ -4,15 +4,17 @@ package main
 import(
 //	"math/rand"
 //	"time"
-//	"fmt"
+	"fmt"
 	"./gamelogic"
-	
-
-
+	"bufio"
+  	"os"
 
 )
 
 func main(){
+	//reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Welcome to Five Card Draw! (press 'enter' between messages to continue)")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
 
 	Red := new(gamelogic.Player)
 	Red.Name = "Red"
@@ -28,17 +30,20 @@ func main(){
 	Green.Bet = 0
 
 	players := make([]gamelogic.Player, 2)
-	players = append(players, *Red)
-	players = append(players, *Green)
+	players[0] = *Red
+	players[1] = *Green
 	ante := 1
 	minBet := 0
 	maxBet := 10
 	dealerToken := 0
 
+	gamelogic.Rand_init()
 
+	fmt.Printf("This prototype game will have two players, Red and Green.")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
 	gamelogic.Game(players, ante, minBet, maxBet, dealerToken)
 
 
-
-
 }
+
+
